a0955487a581c6ac02c9a4e0e6db06652f58fee2
jmbesada <jmbesada.juez@gmail.com>
2024-03-03 20:29:30 +0100

    added arch-unit

--- src/main/java/com/santalucia/categorizer/application/services/impl/CategorizationServiceImpl.java
+++ src/main/java/com/santalucia/categorizer/application/services/impl/CategorizationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.santalucia.categorizer.application.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.santalucia.categorizer.application.api.model.MovementCategorizedResource;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CategorizationServiceImpl implements CategorizationService {
 
+	@Autowired Environment env;
 	@Override
 	public MovementCategorizedResource categorize(MovementResource movementResource) {
 		// TODO Auto-generated method stub
