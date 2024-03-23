00909b3b7a42c8576ad3a72743814ad500792da6
jmbesada <jmbesada.juez@gmail.com>
2024-03-23 21:48:49 +0100

    added h2 for tests

--- src/test/java/com/santalucia/categorizer/CategorizerApplicationTests.java
+++ src/test/java/com/santalucia/categorizer/CategorizerApplicationTests.java
@@ -2,8 +2,11 @@ package com.santalucia.categorizer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class CategorizerApplicationTests {
 
 	@Test
