a20a816975bf60ab870dfbe1ab8194f6a12d7ba4
jmbesada <jmbesada.juez@gmail.com>
2024-08-13 22:43:55 +0200

    added spring cloud vault

--- src/main/java/com/santalucia/categorizer/CategorizerApplication.java
+++ src/main/java/com/santalucia/categorizer/CategorizerApplication.java
@@ -1,10 +1,12 @@
 package com.santalucia.categorizer;
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.retry.annotation.EnableRetry;
 
 import com.santalucia.categorizer.infrastructure.repository.UserRepository;
@@ -24,11 +26,15 @@ public class CategorizerApplication {
 	}
 	
 	@Bean
-	ApplicationListener<ApplicationReadyEvent> ready(UserRepository userRepository){
+	ApplicationListener<ApplicationReadyEvent> ready(UserRepository userRepository,
+			Environment env){
 		return event -> {
 			log.info("AppliucationRadyEvent: {}", 
 					event.getSpringApplication().getMainApplicationClass());
 			log.info("Users loaded: {}", userRepository.findAll());
+			
+			log.info("Username for postgresql retrieved from vault: {}", env.getProperty("database.username"));
+			log.info("Username for kafka retrieved from vault: {}", env.getProperty("kafka.username"));
 		};
 	}
 }
